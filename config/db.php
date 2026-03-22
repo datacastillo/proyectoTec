e482fa164a0c98b5086873a79d48598b1ade1e54
datacastillo <jucarlos893@gmail.com>
2026-03-22 17:37:10 -0600

    conexion unificada

--- config/db.php
+++ config/db.php
@@ -1,12 +1,18 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "control_escolar";
+$user = "root"; 
+$password = ""; 
+$dbname = "control_escolar";
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+// Salida archivos (MySQLi)
+$conexion = mysqli_connect($host, $user, $password, $dbname);
+mysqli_set_charset($conexion, "utf8");
 
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+// Salida archivos de (PDO)
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Error silencioso para no romper mysqli
 }
 ?>
